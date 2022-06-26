668708a6fc56a5503c6fed6e066231406a0b9fdd
Channing Robertson <channing.robertson@gmail.com>
2022-06-26 09:02:47 -0500

    Add project files.

---
+++ TheBlogProjectMVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace TheBlogProjectMVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
